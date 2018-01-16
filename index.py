833f47ca0bf7c240f07d2c859d5f8ad104a1cd0d
dingdinga <ouyanghanjun@wanjizhijia.com>
2018-01-16 17:25:24 +0800

    注释

--- index.py
+++ index.py
@@ -5,20 +5,25 @@ root = Tk()
 root.title("Hello Test")
 root.geometry('800x500+600+200')
 root.iconbitmap('D:\\www\\scrapy\\new\\grammar\\win.ico')
+#上
 frmTop = Frame(root,width=800,height=400,bg='green')
 frmTop.pack(side=TOP)
-
+#上左
 frmTopLeft = Frame(frmTop,width=600,height=400,bg='yellow')
 frmTopLeft.pack(side=LEFT)
 
+#上左上
 frmTopLeftTop = Frame(frmTopLeft,width=600,height=320,bg='grey')
 frmTopLeftTop.pack(side=TOP)
+#上左下
 frmTopLeftFoot = Frame(frmTopLeft,width=600,height=80,bg='purple')
 frmTopLeftFoot.pack(side=TOP)
 
+#上右
 frmTopRight = Frame(frmTop,width=200,height=400,bg='red')
 frmTopRight.pack(side=LEFT)
 
+#下
 frmFoot = Frame(root,width=800,height=100,bg='blue')
 frmFoot.pack(side=TOP)
 
