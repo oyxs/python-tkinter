36824ce733d3a1fd84c935c1582e3f4b1c73c40b
dingdinga <ouyanghanjun@wanjizhijia.com>
2018-01-16 17:21:32 +0800

    布局

--- index.py
+++ index.py
@@ -1,10 +1,41 @@
-import tkinter as tk
-root = tk.Tk()
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+from tkinter import *
+root = Tk()
 root.title("Hello Test")
 root.geometry('800x500+600+200')
 root.iconbitmap('D:\\www\\scrapy\\new\\grammar\\win.ico')
+frmTop = Frame(root,width=800,height=400,bg='green')
+frmTop.pack(side=TOP)
 
-labelHello = tk.Label(root, text = "Hello Tkinter!",bg = "green").pack()
+frmTopLeft = Frame(frmTop,width=600,height=400,bg='yellow')
+frmTopLeft.pack(side=LEFT)
+
+frmTopLeftTop = Frame(frmTopLeft,width=600,height=320,bg='grey')
+frmTopLeftTop.pack(side=TOP)
+frmTopLeftFoot = Frame(frmTopLeft,width=600,height=80,bg='purple')
+frmTopLeftFoot.pack(side=TOP)
+
+frmTopRight = Frame(frmTop,width=200,height=400,bg='red')
+frmTopRight.pack(side=LEFT)
+
+frmFoot = Frame(root,width=800,height=100,bg='blue')
+frmFoot.pack(side=TOP)
 
 
+
+
+
+
+#left
+
+'''Label(frmLeft, text='3333333', font=('Arial', 15)).pack(side=TOP)
+Label(frmLeft, text='3333333333333333', font=('Arial', 15)).pack(side=TOP)'''
+'''Button(root,text='A').pack(side=LEFT,expand=YES,fill=Y)
+Button(root,text='B').pack(side=TOP,expand=YES,fill=BOTH)
+Button(root,text='C').pack(side=RIGHT,expand=YES,fill=NONE)
+Button(root,text='D').pack(side=LEFT,expand=NO,fill=Y)
+Button(root,text='E').pack(side=TOP,expand=YES,fill=BOTH)
+Button(root,text='F').pack(side=BOTTOM,expand=YES)
+Button(root,text='G').pack(anchor=SE)'''
 root.mainloop()
